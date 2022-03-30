eb28bd433920eb9c1a5c366fcbde66ad3da89bc3
Noelle Hernandez <90921971+CodeGirlNH2006@users.noreply.github.com>
2022-03-30 23:33:41 +0000

    Finished

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import math
 
-p = 250000
-r = 4.85
-ny = 30
-nm = ny * 12
-mr = r/(12*100)
-monthlypayment = p * mr * ((1+mr)**ny)/((1+mr)**ny - 1)
-print(monthlypayment)
+principal = 250000.0
+annual_rate = 4.85
+num_of_years = 30.0
+monthly_payment = (annual_rate/100/12) * (1+(annual_rate/100/12))**(num_of_years*12)/((1+(annual_rate/100/12))**(num_of_years*12)-1)*principal
+total_payments = monthly_payment*num_of_years*12
+total_interest = total_payments - principal
+print("Principal = " + str(principal))
+print("Annual Rate = " + str(annual_rate))
+print("Number of Years = " + str(num_of_years))
+print("Monthly Payment = " + str(round(monthly_payment, 2)))
+print("Total Payments = " + str(round(total_payments, 2)))
+print("Total Interest = " + str(round(total_interest, 2)))
